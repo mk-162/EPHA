2ea7aaa3e52fb93ae4808c6a3ab2ddc5b9608598
ZARCG <salvium-team@proton.me>
2025-12-17 19:20:28 +0000

    Fix wouter router base path for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -14,7 +14,7 @@ import ConstructionIndustry from "@/pages/industries/construction";
 import Contact from "@/pages/contact";
 import Distribution from "@/pages/distribution";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -35,8 +35,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Router />
+        <Router base="/EPHA">
+          <Toaster />
+          <AppRouter />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
